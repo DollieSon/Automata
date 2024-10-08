fa80b95dccdd2518283db4206cd2486770fccc0d
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-10-08 23:54:59 +0800

    addded transistion verification

--- DFA String/DFA.py
+++ DFA String/DFA.py
@@ -2,6 +2,7 @@ class DFAStruct():
     def __init__(self,alphabet:list):
         self.alphabet = alphabet
         self.states = {}
+        self.final_state = ''
 
     def add_node(self):
         self.states[str(len(self.states.keys()))] = {}
@@ -14,6 +15,23 @@ class DFAStruct():
             raise Exception(f'P{Transistion} is not in alphabet')
 
         self.states[From][Transistion] = To
+
+    def set_final_state(self,state:str):
+        if state not in self.states.keys():
+            raise Exception(f'{state} does not exist')
+        self.final_state = state
+
+    def verify(self):
+        missing_trans = {}
+        for Node_State in self.states.keys():
+            if Node_State == self.final_state:
+                continue
+            if set(self.states[Node_State].keys()) != set(self.alphabet):
+                missing_trans[Node_State] = set(self.alphabet) - set(self.states[Node_State].keys())
+                continue
+        if missing_trans != {}:
+            raise Exception(f'missing transistions \n {missing_trans}')
+
 
     def printTransistionTable(self):
         print(f'Alphabet = {self.alphabet}')
